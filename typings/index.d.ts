bd390bec11955165da9323554324b8f821904be3
Irmantas Zenkus <irmantas.zenkus@hometogo.com>
2016-11-09 13:38:26 +0200

    Delete index.d.ts

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/jest/index.d.ts" />
-/// <reference path="globals/react-redux/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
-/// <reference path="globals/redux-actions/index.d.ts" />
-/// <reference path="globals/redux/index.d.ts" />
-/// <reference path="globals/superagent/index.d.ts" />
-/// <reference path="modules/react-dom/index.d.ts" />
