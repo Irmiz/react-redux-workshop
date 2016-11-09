2c98fe243514b59d7be8071e77789359c6d28b19
Irmantas Zenkus <irmantas.zenkus@hometogo.com>
2016-11-09 15:23:08 +0200

    Move everything to index file

---
+++ src/scripts/components/Search/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { combineReducers } from 'redux';
+import { reducer as offers } from 'components/OfferList';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const reducers = combineReducers({
+    offers
+});
+let store = createStore(reducers);
+
+// Main container for whole application
+const App = (
+    <Provider store={store}>
+        <div>kkk</div>
+    </Provider>
+);
+
+render(App, document.getElementById('root'));
