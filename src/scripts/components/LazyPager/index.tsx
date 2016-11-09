0a08f0f4dd7f6fe56f535f6f1040841d17d18f61
Kasparas Pochyla <mail.igidas@gmail.com>
2016-11-09 15:44:43 +0200

    Created component

---
+++ src/scripts/components/LazyPager/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+// Main container for whole application
+const App = (
+    <div>Lazy Loader</div>
+);
+
+render(App, document.getElementById('root'));
