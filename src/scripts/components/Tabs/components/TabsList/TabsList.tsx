c3e95df937ff54707cfafcd462af0bfda2f9ec62
Aurelija <aurelija.kunceviciute@hometogo.com>
2016-11-20 20:17:04 +0200

    show active tab content

--- src/scripts/components/Tabs/components/TabsList/TabsList.tsx
+++ src/scripts/components/Tabs/components/TabsList/TabsList.tsx
@@ -2,19 +2,54 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 interface Props {
-    tabs: Array<any>
+    list: Array<any>,
+    activeTabKey: String
 }
 
-export const ImageList: React.StatelessComponent<Props> = props => (
-    <ul>
-        {props.tabs.map((tab, index) => (
-            <li>{tab.name}</li>
-        ))}
-    </ul>
-);
+const ACTIVE_TAB_STYLE = {
+    backgroundColor: '#eee'
+};
 
-const mapStateToProps = (state, ownProps) => ({
-    tabs: state.tabs
-});
+export class ImageList extends React.Component<Props, any> {
+    renderContent(activeTabKey) {
+        switch (activeTabKey) {
+            case 'images':
+                return <div>images</div>
+            case 'amenities':
+                return <div>amenities</div>
+            case 'description':
+                return <div>description</div>
+            default:
+                return <div>oops</div>
+        }
+    }
+
+    render() {
+        const {list, activeTabKey} = this.props;
+        return (
+            <div>
+                <ul>
+                    {list.map((item, index) => (
+                        <li
+                            style={item.key === activeTabKey ? ACTIVE_TAB_STYLE : null}
+                        >
+                            {item.name}
+                        </li>
+                    ))}
+                </ul>
+                <div>
+                    {this.renderContent(activeTabKey)}
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        list: state.tabs.list,
+            activeTabKey: state.tabs.activeTabKey
+    };
+};
 
 export default connect(mapStateToProps)(ImageList);
--- src/scripts/components/Tabs/components/TabsList/index.tsx
+++ src/scripts/components/Tabs/components/TabsList/index.tsx
@@ -10,14 +10,24 @@ type Tabs = Array<any>;
 // Actions
 const OPEN = 'components/Tabs/OPEN';
 
-const defaultState = [
-    {
-        name: 'tab1'
-    },
-    {
-        name: 'tab2'
-    }
-];
+const defaultState = {
+    list: [
+        {
+            name: 'Description',
+            content: 'Te mea veniam consequat, per regione indoctum in. In fierent corrumpit nam, admodum fuisset at sea. Modus ornatus accusata eum ea, nec solum oratio democritum ut.',
+            key: 'description'
+        },
+        {
+            name: 'Images',
+            key: 'images'
+        },
+        {
+            name: 'Amenities',
+            key: 'amenities'
+        }
+    ],
+    activeTabKey: 'description'
+};
 // Reducer
 const reducer = (state = defaultState, action) => {
     switch (action.type) {
