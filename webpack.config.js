169c6a490d12b250d8de5bc77767ce05e43d69a9
aurelijakunceviciute <aurelija.kunceviciute@hometogo.com>
2016-12-02 15:34:33 +0200

    change entry app

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require("path");
 module.exports = {
     entry: {
         search: ["./src/scripts/components/Search"],
-        tabs: ["./src/scripts/components/Tabs/components/App"]
+        tabs: ["./src/scripts/entry/Tabs"]
     },
     resolve: {
         root: [
