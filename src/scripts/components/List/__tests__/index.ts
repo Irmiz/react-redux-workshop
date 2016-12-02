c6d93a2150875aef2e1817b66b7f802fe1c6760e
Linas Jasinauskas <linas.jasinauskas@hometogo.com>
2016-12-02 16:17:19 +0200

    Fixed tests

--- src/scripts/components/List/__tests__/index.ts
+++ src/scripts/components/List/__tests__/index.ts
@@ -3,13 +3,13 @@ import { reducer, RECEIVE } from '../';
 describe('reducer', () => {
     it('returns default state', () => {
         expect(
-            reducer([], { type: 'foobar', payload: { items: ['a', 'b'] } })
+            reducer([], { type: 'foobar', payload: ['a', 'b'] })
         ).toEqual([]);
     });
 
     it('receive items', () => {
         expect(
-            reducer([], { type: RECEIVE, payload: { items: ['a', 'b'] } })
+            reducer([], { type: RECEIVE, payload: ['a', 'b'] })
         ).toEqual(['a', 'b']);
     });
 });
